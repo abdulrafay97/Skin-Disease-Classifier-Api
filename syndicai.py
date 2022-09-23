d067352cc14cc308f3293eeb3dd857a50ec649d9
Abdul Rafay <80894764+abdulrafay97@users.noreply.github.com>
2022-09-23 09:35:27 +0500

    Update syndicai.py

--- syndicai.py
+++ syndicai.py
@@ -2,9 +2,19 @@ import torch
 from torchvision import transforms, models
 import torch.nn as nn
 from PIL import Image
+import requests
+from io import BytesIO
 
 class PythonPredictor:
-    def  __init__(self):
+    def  __init__(self, config):
+        self.normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                    std=[0.229, 0.224, 0.225])
+        
+        self.data_transforms = transforms.Compose([
+                transforms.Resize((224,224)),
+                transforms.ToTensor(),
+                self.normalize
+            ])
         device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
         self.model = models.efficientnet_b2(pretrained=False).to(device)
         in_features = 1024
@@ -19,21 +29,12 @@ class PythonPredictor:
             nn.Dropout(0.4),
             nn.Linear(128, 24),).to(device)
 
-        self.model.load_state_dict(torch.load('D:\FYP-ALL DATA\API\\enet.h5' , map_location=torch.device('cpu')))
+        self.model.load_state_dict(torch.load('D:\syndicai\API\enet.h5' , map_location=torch.device('cpu')))
         self.model.eval()
 
-    def processImage(self, file):
+    def predictLabel(self, file):
         image_size = 224
-        normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
-                                    std=[0.229, 0.224, 0.225])
-        data_transforms = transforms.Compose([
-                transforms.Resize((224,224)),
-                transforms.ToTensor(),
-                normalize
-            ])
-        image = Image.open(file)
-        img = data_transforms(image)
-        img = torch.reshape(img , (1, 3, image_size, image_size))
+        img = torch.reshape(file , (1, 3, image_size, image_size))
         return self.model(img)
 
     def predict(self, img):
@@ -44,7 +45,12 @@ class PythonPredictor:
             'Tinea Corporis', 'Tinea Nigra', 'Tungiasis', 'Epidermolysis Bullosa Pruriginosa',
             'Herpes Simplex', 'Neurofibromatosis', 'Papilomatosis Confluentes And Reticulate',
             'Pediculosis Capitis']
-        out = self.processImage(img)
+        image = requests.get(img["url"]).content
+        img_pil = Image.open(BytesIO(image))
+        img_tensor = self.data_transforms(img_pil)
+        img_tensor.unsqueeze_(0)
+        print(type(img_tensor))
+        out = self.predictLabel(img_tensor)
         _, predicted = torch.max(out.data, 1)
         allClasses.sort()
         labelPred = allClasses[predicted]

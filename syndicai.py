c6c30269aba4284ef23cfb887f92ceed19b91b97
Abdul Rafay <80894764+abdulrafay97@users.noreply.github.com>
2022-09-28 11:12:47 +0500

    Update syndicai.py

--- syndicai.py
+++ syndicai.py
@@ -46,7 +46,7 @@ class PythonPredictor:
             'Tinea Corporis', 'Tinea Nigra', 'Tungiasis', 'Epidermolysis Bullosa Pruriginosa',
             'Herpes Simplex', 'Neurofibromatosis', 'Papilomatosis Confluentes And Reticulate',
             'Pediculosis Capitis']
-        image_str = requests.get(payload["url"]).content
+        image_str = requests.get(payload["url"])
         img_pil = bytes_to_img(image_str[0])
         img_tensor = self.data_transforms(img_pil)
         img_tensor.unsqueeze_(0)
